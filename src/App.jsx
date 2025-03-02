f158d5ab612316560eef05747d0a2ec67f7b1368
mohammad <mohammad19hobbi@gmail.com>
2025-03-02 09:16:19 +0330

    HOME

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Route } from "react-router-dom"
 import SignUp from "./components/auth/signUp"
 import Login from "./components/auth/Login"
 import { Routes } from "react-router-dom"
+import Layout from "./components/home/layout"
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<SignUp />}/>
       <Route path="/login" element={<Login />}/>
-      {/* <Route path="/home" element={home}/> */}
+       <Route path="/home" element={<Layout/>}/> 
     </Routes>
     </BrowserRouter>
   )
---
+++ src/components/home/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SideBar from './sidebar'
+function Layout() {
+  return (
+    <div>
+      {/* <SideBar/> */}
+    </div>
+  )
+}
+
+export default Layout
